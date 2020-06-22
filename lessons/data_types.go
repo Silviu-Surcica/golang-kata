b4061611721db6233e0173e0dea408abb9a0ed61
Silviu Surcica <silviu.surcica@solarwinds.com>
2020-06-22 16:39:47 +0300

    push exercise

--- lessons/data_types.go
+++ lessons/data_types.go
@@ -98,20 +98,20 @@ func Maps(){
 }
 
 /* ===========EXERCISE===========
-Implement the function DigitalRoot
-It takes an int and returns an int.
-Given n, take the sum of the digits of n.
-If that value has more than one digit,
-continue reducing in this way until a single-digit number is produced.
-This is only applicable to the natural numbers.
+Implement the function FindOutlier
+It takes an array of ints and returns an int.
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised of even integers
+except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
 
 Helper function:
 s := strconv.Itoa(20) // "20"
 i, err := strconv.Atoi("20") // 20
 
 Ex:
- 16  -->  1 + 6 = 7
-   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
-132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
-493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
 */
--- tests/data_types_test.go
+++ tests/data_types_test.go
@@ -1,25 +1,93 @@
 package lessons_test
 
 import (
+   "math"
+   "math/rand"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"golang-kata/lessons"
 )
 
-var _ = Describe("DataTypes", func() {
-    It("Test 12", func() {
-    Expect(lessons.DigitalRoot(12)).To(Equal(3))
+type testRecord struct {
+  integers []int
+  outlier  int
+}
+
+var _ = Describe("Basic tests", func() {
+  It("Odd at the back", func() {
+    Expect(lessons.FindOutlier([]int{2, 6, 8, 10, 3})).To(Equal(3))
   })
-  It("Test 195", func() {
-    Expect(lessons.DigitalRoot(195)).To(Equal(6))
+  It("Odd in the middle", func() {
+    Expect(lessons.FindOutlier([]int{2, 6, 8, 200, 700, 1, 84, 10, 4})).To(Equal(1))
   })
-  It("Test 993", func() {
-    Expect(lessons.DigitalRoot(993)).To(Equal(3))
+  It("Odd in the front", func() {
+    Expect(lessons.FindOutlier([]int{17, 6, 8, 10, 6, 12, 24, 36})).To(Equal(17))
   })
-    It("Test 167346", func() {
-        Expect(lessons.DigitalRoot(167346)).To(Equal(9))
-      })
-      It("Test 0", func() {
-        Expect(lessons.DigitalRoot(0)).To(Equal(0))
-      })
+  It("Even in the front", func() {
+    Expect(lessons.FindOutlier([]int{2, 1, 7, 17, 19, 211, 7})).To(Equal(2))
+  })
+  It("Even in the middle", func() {
+    Expect(lessons.FindOutlier([]int{1, 1, 1, 1, 1, 44, 7, 7, 7, 7, 7, 7, 7, 7})).To(Equal(44))
+  })
+  It("Even at the end", func() {
+    Expect(lessons.FindOutlier([]int{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 35, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 7, 7, 7, 7, 1000})).To(Equal(1000))
+  })
+  It("Odd at the back, negative", func() {
+    Expect(lessons.FindOutlier([]int{2, -6, 8, -10, -3})).To(Equal(-3))
+  })
+  It("Odd in the middle, negative", func() {
+    Expect(lessons.FindOutlier([]int{2, 6, 8, 2, -66, 34, -35, 66, 700, 1002, -84, 10, 4})).To(Equal(-35))
+  })
+  It("Odd in the front, negative", func() {
+    Expect(lessons.FindOutlier([]int{-1 * math.MaxInt32, -18, 6, -8, -10, 6, 12, -24, 36})).To(Equal(-1 * math.MaxInt32))
+  })
+  It("Even in the front, negative", func() {
+    Expect(lessons.FindOutlier([]int{-20, 1, 7, 17, 19, 211, 7})).To(Equal(-20))
+  })
+  It("Even in the middle, negative", func() {
+    Expect(lessons.FindOutlier([]int{1, 1, -1, 1, 1, -44, 7, 7, 7, 7, 7, 7, 7, 7})).To(Equal(-44))
+  })
+  It("Odd answer, zeroes", func() {
+    Expect(lessons.FindOutlier([]int{1, 0, 0})).To(Equal(1))
+  })
+  It("Even in the middle, zero", func() {
+    Expect(lessons.FindOutlier([]int{3, 7, -99, 81, 90211, 0, 7})).To(Equal(0))
+  })
+})
+
+func randOdd(n int) int {
+  return rand.Intn(n/2-1)*2 + 1
+}
+
+func randEven(n int) int {
+  return rand.Intn(n/2) * 2
+}
+
+func randomTest(size, maxVal int) {
+  posA := rand.Intn(size)
+  posB := rand.Intn(size)
+  odds := make([]int, size)
+  evens := make([]int, size)
+  for i := 0; i < size; i++ {
+    odds[i] = randOdd(maxVal)
+    evens[i] = randEven(maxVal)
+  }
+  oddVal := randOdd(maxVal)
+  evenVal := randEven(maxVal)
+  odds[posA] = evenVal
+  evens[posB] = oddVal
+  Expect(lessons.FindOutlier(odds)).To(Equal(evenVal))
+  Expect(lessons.FindOutlier(evens)).To(Equal(oddVal))
+}
+
+var _ = Describe("Random tests", func() {
+  It("Small array test", func() {
+    randomTest(400, 3000)
+  })
+  It("Medium array test", func() {
+    randomTest(1000, 30000)
+  })
+  It("Large array test", func() {
+    randomTest(100000, 100000)
+  })
 })
